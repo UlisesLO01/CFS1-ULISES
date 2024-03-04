2e0c7e5103294339430ab96dc338d549e4dd81ee
UlisesLO01 <olivaresulisesleonardo@gmail.com>
2024-03-04 19:00:43 -0300

    Mi primer commit

---
+++ CLASE 1/Ejercicio_1.ts
@@ -0,0 +1,13 @@
+console.log("<> PASOS PARA ARMAR UNA CARPA <>");
+console.log("");
+console.log("Paso 1: ELEGIR UN LUGAR ADECUADO Y COMODO");
+console.log("Hola 2: ABRIR LA BOLSA DE LA CARPA");
+console.log("Hola 3: SACAR TODAS QUE TIENE LA BOLSA");
+console.log("Hola 4: LEER LAS INSTRUCCIONES");
+console.log("Hola 5: EXTENDER LA CARPA EN EL PISO");
+console.log("Hola 6: PONER LOS CAÑOS QUE SOSTIENEN LA CARPA");
+console.log("Hola 7: CLAVAR LAS ESTACAS DE LA CARPA AL SUELO");
+console.log("Hola 8: ATAR LAS SOGAS A LUGARES FIRMES ");
+console.log("Hola 9: INFLAR EL COLCHON INFLABLE ");
+console.log("Hola 10: ¡¡ DISFRUTAR !!");
+
